9cde628985c5c831045a5e1250123fd6e82d475f
AnneSophie148 <115065798+AnneSophie148@users.noreply.github.com>
2022-11-10 11:38:16 +0100

    Delete script directory

--- script/scriptexo2.sh
+++
@@ -1,5 +0,0 @@
-ANNEE=$1
-MOIS=$2
-NB_LIEUX=$3
-echo "pour la date $ANNEE $MOIS les 10 lieux les plus cités seront $NB_LIEUX " > sortie.txt
-grep "Location" $ANNEE"_"$MOIS*.ann | sort | head -n $NB_LIEUX >> sortie.txt
